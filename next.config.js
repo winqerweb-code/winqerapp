7774742eaf9b0d0ab94178475b37c07feea4e86a
Winqer Deployer <deploy@winqer.app>
2026-01-14 10:18:48 +0900

    fix(config): Correctly enable serverActions in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        serverActions: {
-            bodySizeLimit: '10mb',
-        },
+        serverActions: true,
     },
 }
 
